1a65b6e9d1970608e82aa6333403cda117464722
chenjiandongx <chenjiandongx@qq.com>
2019-10-09 15:05:01 +0800

    Update: 新增项目文件

---
+++ cutecharts/globals.py
@@ -0,0 +1,17 @@
+import os
+
+from jinja2 import Environment, FileSystemLoader
+
+
+class _CurrentConfig:
+    GLOBAL_ENV = Environment(
+        keep_trailing_newline=True,
+        trim_blocks=True,
+        lstrip_blocks=True,
+        loader=FileSystemLoader(
+            os.path.join(os.path.abspath(os.path.dirname(__file__)), "templates")
+        ),
+    )
+
+
+CurrentConfig = _CurrentConfig()
