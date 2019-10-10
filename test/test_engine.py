c9c372f95ff377a7bda27648e0ce984889d850b7
chenjiandongx <chenjiandongx@qq.com>
2019-10-10 11:43:40 +0800

    Add: 新增测试

--- test/test_engine.py
+++ test/test_engine.py
@@ -9,3 +9,10 @@ def test_engine_render():
     html = basic.render()
     assert_in(AssetsHost.DEFAULT_HOST, html)
     assert_in("chartXkcd", html)
+
+
+def test_engine_render_notebook():
+    basic = BasicChart()
+    html = basic.render_notebook().__html__()
+    assert_in(AssetsHost.DEFAULT_HOST, html)
+    assert_in("chartXkcd", html)
