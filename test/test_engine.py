02e7f63f427d2e9f3f27341413b5adbfd78cc458
chenjiandongx <chenjiandongx@qq.com>
2019-10-13 15:16:31 +0800

    Update: docs and tests

--- test/test_engine.py
+++ test/test_engine.py
@@ -1,6 +1,7 @@
-from nose.tools import assert_in
+from nose.tools import assert_equal, assert_in
 
 from cutecharts.charts.basic import BasicChart
+from cutecharts.faker import Faker
 from cutecharts.globals import AssetsHost
 
 
@@ -16,3 +17,9 @@ def test_engine_render_notebook():
     html = basic.render_notebook().__html__()
     assert_in(AssetsHost.DEFAULT_HOST, html)
     assert_in("chartXkcd", html)
+
+
+def test_faker():
+    attrs = Faker.choose()
+    values = Faker.values()
+    assert_equal(len(attrs), len(values))
